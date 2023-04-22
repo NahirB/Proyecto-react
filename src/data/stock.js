6dfcbc978b3d6bcb6cc7a3d304babf06bd17aed8
NahirB <nahirbts@outlook.com>
2023-04-22 16:16:42 -0300

    cambio estilo

--- src/data/stock.js
+++ src/data/stock.js
@@ -3,42 +3,42 @@ export const stock = [
         id: 1,
         description: "Notas con animación",
         price: 35,
-        image: ['img/Notas-animadas50x50.jpg'],
+        image: ['img/Notas-ani-grande.jpg'],
         category: "papelera"
     },
     {
         id: 2,
         description: "Notas lisas",
         price: 352,
-        image: ['img/Bloc-notas-lisas50x50.jpg'],
+        image: ['img/Bloc-lisas-grande.jpg'],
         category: "papelera"
     },
     {
         id: 3,
         description: "Cinta adhesiva",
         price: 200,
-        image: ['img/Cinta-adhesiva50x50.jpg'],
+        image: ['img/Cintaadhesiva-grande.jpg'],
         category: "accesorios"
     },
     {
         id: 4,
         description: "Adhesivo marcador",
         price: 150,
-        image: ['img/Adhesivo-color-regla_50x50.jpg'],
+        image: ['img/Adhesivas grande.jpg'],
         category: "accesorios"
     },
     {
         id: 5,
         description: "Resaltador doble punta",
         price: 352,
-        image: ['img/Resaltador-50x50.jpg'],
+        image: ['img/Resaltador doble punta grande.jpg'],
         category: "accesorios"
     },
     {
         id: 6,
         description: "Rotulador",
         price: 200,
-        image: ['img/Rotulador-50x50.jpg'],
+        image: ['img/Rotuladores-punta.jpg'],
         category: "accesorios"
     },
 ]
