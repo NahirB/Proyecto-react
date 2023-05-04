51f3511033bb4a2cf8f4b6bac17b0c86db16519f
NahirB <nahirbts@outlook.com>
2023-05-04 20:14:04 -0300

    subo funcionalidad al carrito

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
-import { Card, Button } from 'react-bootstrap'
-import { ItemCount } from '../ItemCount/ItemCount'
+import React, { useContext, useState } from 'react';
+import {useNavigate} from 'react-router-dom';
+import { Card, Button } from 'react-bootstrap';
+import { ItemCount } from '../ItemCount/ItemCount';
+import { CartContext } from '../context/CartContext';
 
-export const ItemDetail = ({ id, description, price, image, category }) => {
+export const ItemDetail = ({ id, description, price, image, category, stock }) => {
   // Pasamos mediante props, cada propiedad de nuestro productos(objetos) y lo colocamos por props en nuestro return
+  
+  const navigate = useNavigate ()
+
+  const volverParaAtras = () =>{
+
+    navigate(-1)
+  }
+
+  //__________________________________________
+  const {addToCart} =useContext (CartContext)
+  //__________________________________________
+  
+  const [counter, setCounter] = useState(0)
+
+  const sumarAlCarrito = () =>{
+    const newItem = {
+      id,
+      description,
+      price,
+      image,
+      category,
+      counter
+    }
+    console.log(newItem)
+  addToCart(newItem)
+  }
+  
+  
   return (
     <div className='item'>
       <Card style={{ width: '25rem' }}>
@@ -12,11 +42,15 @@ export const ItemDetail = ({ id, description, price, image, category }) => {
           <Card.Title>{id}</Card.Title>
           <Card.Title>{description}</Card.Title>
           <Card.Title>{price}</Card.Title>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea error totam quibusdam doloribus, alias aperiam exercitationem laboriosam illum similique eos, delectus vitae odit maxime, repellendus iusto quisquam placeat blanditiis. Cupiditate! </p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Ea error totam quibusdam doloribus, alias aperiam exercitationem 
+            laboriosam illum similique eos, delectus vitae odit maxime, 
+            repellendus iusto quisquam placeat blanditiis. Cupiditate! </p>
           <Card.Title>Categoria: {category}</Card.Title>
-          <ItemCount />
-          <Button variant="primary">Agregar al carrito</Button>
+          <ItemCount max={stock} modify={setCounter} cantidad={counter} />
+          <Button onClick={sumarAlCarrito}>Agregar al carrito</Button>
         </Card.Body>
+        <Button onClick={volverParaAtras} className='btnbtn-sucess'>Volver atrás</Button>
       </Card>
     </div>
   )
--- src/data/stock.js
+++ src/data/stock.js
@@ -1,44 +1,82 @@
 export const stock = [
     {
         id: 1,
-        description: "Notas con animación",
-        price: 35,
-        image: ['img/Notas-ani-grande.jpg'],
-        category: "papelera"
+        description: "Notas con detalle",
+        price: 400,
+        image: '../img/NotaAnimadaGrande.jpg',
+        category: "papelera",
+        stock: 20
     },
+
     {
         id: 2,
         description: "Notas lisas",
         price: 352,
-        image: ['img/Bloc-lisas-grande.jpg'],
-        category: "papelera"
+        image: '../img/Bloc-lisas-grande.jpg',
+        category: "papelera",
+        stock: 20
     },
+
     {
         id: 3,
         description: "Cinta adhesiva",
         price: 200,
-        image: ['img/Cintaadhesiva-grande.jpg'],
-        category: "accesorios"
+        image: '../img/Cintaadhesiva-grnade.jpg',
+        category: "accesorios",
+        stock: 20
     },
+
     {
         id: 4,
         description: "Adhesivo marcador",
         price: 150,
-        image: ['img/Adhesivas grande.jpg'],
-        category: "accesorios"
+        image: '../img/AdhesivasGrande.jpg',
+        category: "accesorios",
+        stock: 20
     },
+
     {
         id: 5,
         description: "Resaltador doble punta",
         price: 352,
-        image: ['img/Resaltador doble punta grande.jpg'],
-        category: "accesorios"
+        image:'../img/ResaltadorDPunta.jpg',
+        category: "accesorios",
+        stock: 20
     },
+
     {
         id: 6,
         description: "Rotulador",
         price: 200,
-        image: ['img/Rotuladores-punta.jpg'],
-        category: "accesorios"
+        image: '../img/RotuladorPincel.jpg',
+        category: "accesorios",
+        stock: 20
+    },
+
+    {
+        id: 7,
+        description: "Notas papel madera",
+        price: 390,
+        image: '../img/NotaHojaMarron-grande.jpg',
+        category: "papelera",
+        stock: 20
+    },
+
+    {
+        id: 8,
+        description: "Notas transparentes",
+        price: 275,
+        image: '../img/Notas_Trans_Ej-gr.jpg',
+        category: "papelera",
+        stock: 20
+    },
+
+    {
+        id: 9,
+        description: "Planner",
+        price: 520,
+        image: '../img/AgendaPlanerPeque-gr.jpg',
+        category: "papelera",
+        stock: 20
     },
 ]
